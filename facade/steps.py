bffd72b0f1b60a7366d00a7e1b2a727ab77571ff
emrah dautbegovic <emrahdautbegovic@MacBook-Air.local>
2025-06-20 13:44:07 +0200

    another attempt

--- facade/steps.py
+++ facade/steps.py
@@ -3,3 +3,4 @@ def step(action, error_message='Something went wrong...'):
         action()
     except Exception:
         print(error_message)
+        raise  # <-- re-raise to fail the test immediately
